262295656caaa3363333e0cd29aad785e2607a77
kodenops <segun.femifadiya@gmail.com>
2024-06-13 14:24:45 +0100

    updated the size for the hot product

--- src/data/hot_products.js
+++ src/data/hot_products.js
@@ -5,34 +5,34 @@ const hotProductsDB = [
 		price: 22.3,
 		oldprice: 75.29,
 		description:
-			"Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-		category: "dress",
+			'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
+		category: 'dress',
 		image:
-			"https://a.1stdibscdn.com/monique-lhuillier-size-10-red-black-wool-lycra-abstract-sheath-dress-for-sale/v_1622/v_171584221664645827568/v_17158422_1664645828259_bg_processed.jpg?width=768",
+			'https://a.1stdibscdn.com/monique-lhuillier-size-10-red-black-wool-lycra-abstract-sheath-dress-for-sale/v_1622/v_171584221664645827568/v_17158422_1664645828259_bg_processed.jpg?width=768',
 		rating: { rate: 4.1, count: 259 },
 	},
 	{
 		id: 2,
-		title: "Varsace Heels",
+		title: 'Varsace Orange Heels',
 		price: 55.99,
 		oldprice: 95.99,
 		description:
-			"great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-		category: "Shoes",
+			'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
+		category: 'Shoes',
 		image:
-			"https://images.dsw.com/is/image/DSWShoes/514130_801_ss_01?impolicy=colpg&imwidth=400&imdensity=1",
+			'https://images.dsw.com/is/image/DSWShoes/514130_801_ss_01?impolicy=colpg&imwidth=400&imdensity=1',
 		rating: { rate: 4.7, count: 500 },
 	},
 	{
 		id: 3,
-		title: "Victoria Secret Bikini",
+		title: 'Victoria Secret Bikini',
 		price: 15.99,
 		oldprice: 45.99,
 		description:
-			"The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-		category: "Bikini",
+			'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.',
+		category: 'Bikini',
 		image:
-			"https://cdn.shopify.com/s/files/1/0624/2526/8462/products/all-over-print-recycled-high-waisted-bikini-white-left-back-636ab1253a26d_1024x1024.jpg?v=1667936580",
+			'https://cdn.shopify.com/s/files/1/0624/2526/8462/products/all-over-print-recycled-high-waisted-bikini-white-left-back-636ab1253a26d_1024x1024.jpg?v=1667936580',
 		rating: { rate: 2.1, count: 430 },
 	},
 	{
@@ -42,9 +42,9 @@ const hotProductsDB = [
 		oldprice: 1000,
 		description:
 			"From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-		category: "Shirts",
+		category: 'Shirts',
 		image:
-			"https://img.freepik.com/premium-photo/elegant-young-woman-white-shirt-black-trousers-with-dark-hair-holding-black-glasses-white-background-studio_567313-9194.jpg",
+			'https://img.freepik.com/premium-photo/elegant-young-woman-white-shirt-black-trousers-with-dark-hair-holding-black-glasses-white-background-studio_567313-9194.jpg',
 		rating: { rate: 4.6, count: 400 },
 	},
 	{
@@ -54,21 +54,21 @@ const hotProductsDB = [
 		oldprice: 1000,
 		description:
 			"From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-		category: "Jacket",
+		category: 'Jacket',
 		image:
-			"https://cdn.shopify.com/s/files/1/0009/5895/5638/products/1_a82b3ca5-ce6f-4cd8-8297-84f14d77a3b9_800x.jpg?v=1596030687",
+			'https://cdn.shopify.com/s/files/1/0009/5895/5638/products/1_a82b3ca5-ce6f-4cd8-8297-84f14d77a3b9_800x.jpg?v=1596030687',
 		rating: { rate: 4.6, count: 400 },
 	},
 	{
 		id: 6,
-		title: "Pure Diamond Earring",
+		title: 'Pure Diamond Earring',
 		price: 295,
 		oldprice: 600,
 		description:
 			"From our Legends Collection, the Naga was inspired by the mythical water dragon that protects the ocean's pearl. Wear facing inward to be bestowed with love and abundance, or outward for protection.",
-		category: "jewelery",
+		category: 'jewelery',
 		image:
-			"https://image.made-in-china.com/44f3j00COIPBUMqCiRL/Fresh-Water-Pearl-925-Silver-Rhodium-Plated-Trendy-2023-Women-Jewellery-6A-CZ-Diamond-Factory-Wholesale-Fine-Jewelry-Earrings.webp",
+			'https://image.made-in-china.com/44f3j00COIPBUMqCiRL/Fresh-Water-Pearl-925-Silver-Rhodium-Plated-Trendy-2023-Women-Jewellery-6A-CZ-Diamond-Factory-Wholesale-Fine-Jewelry-Earrings.webp',
 		rating: { rate: 4.6, count: 400 },
 	},
 ];
--- src/pages/TopProduct.jsx
+++ src/pages/TopProduct.jsx
@@ -1,34 +1,36 @@
-import React from "react";
-import hotProductsDB from "../data/hot_products";
-import Titles from "@/components/Titles";
+import React from 'react';
+import hotProductsDB from '../data/hot_products';
+import Titles from '@/components/Titles';
 const TopProduct = () => {
 	return (
-		<div className=" md:px-[50px] px-[10px] my-[100px] mb-[40px]" id="topProd">
+		<div
+			className=' md:px-[50px] px-[10px] my-[100px] mb-[40px]'
+			id='topProd'>
 			<Titles
-				title="What Others Are buying"
-				subtext="Don’t be left out. Checkout top products other Diva are adding to cart"
+				title='What Others Are buying'
+				subtext='Don’t be left out. Checkout top products other Diva are adding to cart'
 			/>
 			{/* the entire product box */}
-			<div className="product_Container flex flex-wrap gap-[24px] mt-[40px] justify-around items-center">
+			<div className='product_Container flex flex-wrap mt-[40px] justify-around items-center'>
 				{hotProductsDB.map((hotDB) => {
 					return (
 						<div
 							key={hotDB.id}
-							className="md:w-[200px] w-[160px] mb-[40px] hover:shadow-md shadow-sm cursor-pointer p-[10px] rounded-md duration-1000">
+							className='md:w-[200px] w-[120px]  mb-[40px] hover:shadow-md shadow-sm cursor-pointer p-[10px] rounded-md duration-1000'>
 							<img
 								src={hotDB.image}
-								alt=""
-								className="md:w-[200px] md:h-[200px] w-[160px] h-[160px] object-cover aspect-"
+								alt=''
+								className='md:w-[200px] md:h-[200px] w-[160px] h-[160px] object-cover aspect-'
 							/>
-							<div className="text-[10px] mt-[10px] md:text-left text-center">
-								<h4 className=" font-bold">{hotDB.title}</h4>
-								<p className="text-[gray]">{hotDB.category}</p>
-								<div className="prices   md:justify-left justify-center">
-									<span className="newprice text-[var(--primary-color)] font-semibold">
+							<div className='text-[10px] mt-[10px] md:text-left text-center'>
+								<h4 className=' font-bold'>{hotDB.title}</h4>
+								<p className='text-[gray]'>{hotDB.category}</p>
+								<div className='prices   md:justify-left justify-center'>
+									<span className='newprice text-[var(--primary-color)] font-semibold'>
 										${hotDB.price}
 									</span>
-									<span className="mx-[10px]">|</span>
-									<span className="oldprice text-[gray] line-through">
+									<span className='mx-[10px]'>|</span>
+									<span className='oldprice text-[gray] line-through'>
 										${hotDB.oldprice}
 									</span>
 								</div>
