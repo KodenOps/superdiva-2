904c07f7c79f0024a8669fcaee79c8f2ded5a8ff
kodenops <segun.femifadiya@gmail.com>
2024-06-13 14:31:06 +0100

    updated other section

--- src/pages/Promosection.jsx
+++ src/pages/Promosection.jsx
@@ -1,40 +1,43 @@
-import Titles from "@/components/Titles";
-import React from "react";
-import Promo from "@/data/Promo";
-import Link from "next/link";
-import ProductPreview from "./ProductPreview";
+import Titles from '@/components/Titles';
+import React from 'react';
+import Promo from '@/data/Promo';
+import Link from 'next/link';
+import ProductPreview from './ProductPreview';
 
 const Promosection = ({ previewItems, setpreviewItems }) => {
 	return (
 		<div className='min-h-[50vh] py-[10px] md:px-[50px] px-[10px] mt-[10px]" id="topProd'>
 			<Titles
-				title="Our 50% Price Slash Promo"
-				subtext="Explore our catalog of clearance sales. The products are carefully selected to meet your fashion need while also removing 50% off the initial price"
+				title='Our 50% Price Slash Promo'
+				subtext='Explore our catalog of clearance sales. The products are carefully selected to meet your fashion need while also removing 50% off the initial price'
 			/>
 			{/* the entire product box */}
-			<div className="product_Container flex flex-wrap gap-[24px] mt-[40px] justify-around items-center">
+			<div className='product_Container flex flex-wrap gap-[24px] mt-[40px] justify-around items-center'>
 				{Promo.map((hotDB) => {
 					return (
-						<Link href="/ProductPreview" key={hotDB.id} prefetch={false}>
+						<Link
+							href='/ProductPreview'
+							key={hotDB.id}
+							prefetch={false}>
 							<div
-								className="md:w-[200px] w-[160px] mb-[40px] hover:shadow-md shadow-sm cursor-pointer p-[10px] rounded-md duration-1000"
+								className='md:w-[200px] w-[150px] mb-[40px] hover:shadow-md shadow-sm cursor-pointer p-[10px] rounded-md duration-1000'
 								onClick={() => {
 									setpreviewItems(hotDB);
 								}}>
 								<img
 									src={hotDB.image}
-									alt="image of products"
-									className="md:w-[200px] md:h-[200px] w-[160px] h-[160px] object-cover "
+									alt='image of products'
+									className='md:w-[200px] md:h-[200px] w-[160px] h-[160px] object-cover '
 								/>
-								<div className="text-[10px] mt-[10px] md:text-left text-center">
-									<h4 className=" font-bold">{hotDB.title}</h4>
-									<p className="text-[gray]">{hotDB.category}</p>
-									<div className="prices   md:justify-left justify-center">
-										<span className="newprice text-[var(--primary-color)] font-semibold">
+								<div className='text-[10px] mt-[10px] md:text-left text-center'>
+									<h4 className=' font-bold'>{hotDB.title}</h4>
+									<p className='text-[gray]'>{hotDB.category}</p>
+									<div className='prices   md:justify-left justify-center'>
+										<span className='newprice text-[var(--primary-color)] font-semibold'>
 											${hotDB.price}
 										</span>
-										<span className="mx-[10px]">|</span>
-										<span className="oldprice text-[gray] line-through">
+										<span className='mx-[10px]'>|</span>
+										<span className='oldprice text-[gray] line-through'>
 											${hotDB.oldprice}
 										</span>
 									</div>
