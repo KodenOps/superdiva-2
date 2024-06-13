186c18a84f183b8df79410e8b206b44778d6f5ae
kodenops <segun.femifadiya@gmail.com>
2024-06-13 21:09:42 +0100

    update

--- src/pages/ProductPreview.jsx
+++ src/pages/ProductPreview.jsx
@@ -10,20 +10,22 @@ const ProductPreview = ({
 	EachItem,
 	setEachItem,
 }) => {
-	return (
+	return Object.keys(EachItem).length > 0 ? (
 		<div className='min-h-[100vh] '>
 			<Navbar />
 			<div className='min-h-screen'>
-				{/* <Image
+				<Image
 					src={EachItem.image}
 					width={60}
 					height={60}
 					alt='hdhd'
-				/> */}
+				/>
 				<button onClick={(e) => setpreviewItems(!previewItems)}>Click</button>
 				<p>{EachItem.title}</p>
 			</div>
 		</div>
+	) : (
+		alert('System Error: Kindly go back and try again')
 	);
 };
 
