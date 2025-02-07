52ba70de361fc1a96e4d719110760a9fcc559d38
kodenops <segun.femifadiya@gmail.com>
2025-02-07 08:27:17 +0100

    updated IP

--- src/apm.js
+++ src/apm.js
@@ -2,7 +2,7 @@ import { init as initApm } from '@elastic/apm-rum'
 var apm = initApm({
   serviceName: 'superdiva-2',
 
-  serverUrl: 'http://192.168.34.223:8200',
+  serverUrl: 'http://localhost:8200',
 
   serviceVersion: '',
 
